6897e67f11629dd690ba3b21527ca7c661922fca
Zakiuddin53 <zakiuddin810@gmail.com>
2024-11-01 19:43:48 +0530

    update hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/no-unescaped-entities */
-
 import DescriptionOne from "@/components/DescriptionOne";
 import DescriptionTwo from "@/components/DescriptionTwo";
 import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 import Integration from "@/components/integration";
 
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "RigidPace",
-};
-
 export default function Page() {
   return (
     <>
       <Hero />
-      <Integration/>
+      <Integration />
       <DescriptionOne />
       <Products />
-      <DescriptionTwo/>
+      <DescriptionTwo />
     </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,51 +1,79 @@
 import React from "react";
+import Image from "next/image";
+import "./Hero.css";
 
 export default function Hero() {
   return (
-    <div
-      className="zubuz-hero-section"
-      style={{ backgroundImage: "url(assets/images/v1/hero-shape1.png)" }}
-    >
+    <div className="zubuz-hero-section">
       <div className="container">
-        <div className="zubuz-hero-content center position-relative">
-          <h2>
+        <div className="zubuz-hero-content center">
+          <h1>
             Innovate, Integrate, Elevate <br /> Your IT Solution Partner
-          </h2>
-          <p>
-          Ready to take your business to the next level?
-          </p>
-          <div className="zubuz-hero-btn-wrap center">
-            <a className="zubuz-default-btn" href="contact-us.html">
-              {" "}
-              <span>Start Your Free Trial</span>{" "}
-            </a>
-            {/* <a
-              className="video-init zubuz-hero-video"
-              href="https://www.youtube.com/watch?v=SixdAQtWJQ8"
-            >
-              <img src="assets/images/v1/play-btn.png" alt="" />
-              Watch A Demo
-            </a> */}
-          </div>
-          <div className="zubuz-hero-shape">
-            <img src="assets/images/v1/shape.png" alt="" />
+            <span className="zubuz-title-shape">
+              <Image
+                src="/assets/images/v3/shape-v3-01.png"
+                alt=""
+                width={350}
+                height={350}
+              />
+            </span>
+          </h1>
+          <p>Ready to take your business to the next level?</p>
+          <div className="zubuz-extara-mt">
+            <div className="zubuz-subscribe-three">
+              <form action="#">
+                <input type="email" placeholder="Enter your e-mail address" />
+                <button
+                  className="zubuz-default-btn zubuz-subscription-btn three"
+                  id="zubuz-subscription-btn"
+                  type="submit"
+                >
+                  <span>Get started</span>
+                </button>
+                <div className="zubuz-input-email">
+                  <Image
+                    src="/assets/images/icon/email2.svg"
+                    alt=""
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="zubuz-hero-bottom">
-          <div className="zubuz-hero-thumb wow fadeInUpX">
-            <img src="assets/images/v1/hero-mocup1.png" alt="" />
-          </div>
-          <div className="zubuz-hero-card card1 wow zoomIn">
-            <img src="assets/images/v1/h-card1.png" alt="" />
-          </div>
-          <div className="zubuz-hero-card card2 wow zoomIn">
-            <img src="assets/images/v1/h-card2.png" alt="" />
-          </div>
-          <div className="zubuz-hero-card card3 wow zoomIn">
-            <img src="assets/images/v1/h-card4.png" alt="" />
-          </div>
-          <div className="zubuz-hero-card card4 wow zoomIn">
-            <img src="assets/images/v1/h-card3.png" alt="" />
+        <div className="zubuz-border-btoom">
+          <div className="zubuz-hero-thumb3">
+            <Image
+              src="/assets/images/v3/thumb-v3-01.png"
+              alt=""
+              width={600}
+              height={400}
+            />
+            <div className="zubuz-hero-thumb-card1">
+              <Image
+                src="/assets/images/v3/card-v3-1.png"
+                alt=""
+                width={200}
+                height={150}
+              />
+            </div>
+            <div className="zubuz-hero-thumb-card2">
+              <Image
+                src="/assets/images/v3/card-v3-2.png"
+                alt=""
+                width={200}
+                height={150}
+              />
+            </div>
+            <div className="zubuz-hero-thumb-card3">
+              <Image
+                src="/assets/images/v3/card-v3-3.png"
+                alt=""
+                width={200}
+                height={150}
+              />
+            </div>
           </div>
         </div>
       </div>
